e45845c228552fba309094725d06c47b2794ee00
Javier Moncada <javierandresmd@ufps.edu.co>
2021-02-22 20:51:18 -0500

    Eliminando registros de prueba y cambiando el nombre de bd

--- src/main/java/com/ufps/springboot/webflux/app/SpringBootWebfluxApplication.java
+++ src/main/java/com/ufps/springboot/webflux/app/SpringBootWebfluxApplication.java
@@ -1,11 +1,14 @@
 package com.ufps.springboot.webflux.app;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 
 import com.ufps.springboot.webflux.app.models.dao.ProductoDao;
 import com.ufps.springboot.webflux.app.models.documents.Producto;
@@ -20,6 +23,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	
 	private static final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 	
+	@Autowired
+	private ReactiveMongoTemplate mongoTemplate;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebfluxApplication.class, args);
 	}
@@ -27,7 +33,7 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-		
+		mongoTemplate.dropCollection("productos").subscribe();
 		
 		Flux.just( new Producto("TV SONY", 3445.6),
 				   new Producto("PC Acer", 8556.4),
@@ -36,7 +42,11 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 				   new Producto("Ram Crucial", 98645.6)
 				
 				)
-		.flatMap(producto -> productoDao.save(producto))
+		.flatMap(producto -> {
+			producto.setCreateAt(new Date());
+			return productoDao.save(producto);
+			
+					})
 		.subscribe(producto -> log.info("insert: " + producto.getId().concat(" - ").concat(producto.getNombre()))); 
 		
 	}
